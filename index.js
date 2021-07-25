0d51c7e11e0d7a7d062c5c5b1042422e29260193
Grzegorz <grzegorz914@icloud.com>
2021-07-25 21:45:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2374,12 +2374,24 @@ class envoyDevice {
         const updateStatus = ENVOY_API_CODE[homeData.data.update_status] || 'undefined';
 
         //wireless connection kit
-        const wirelessConnectionsCount = (enchargesCount > 0 || enpowersCount > 0) ? homeData.data.wireless_connection.length : 0;
+        if (enchargesCount > 0 || enpowersCount > 0) {
+             this.wirelessConnectionSignalStrength = new Array();
+             this.wirelessConnectionSignalStrengthMax = new Array();
+             this.wirelessConnectionType = new Array();
+             this.wirelessConnectionConnected = new Array();
+          
+          const wirelessConnectionsCount = homeData.data.wireless_connection.length;
         for (let i = 0; i < wirelessConnectionsCount; i++) {
-          const wirelessConnectionSignalStrength = (homeData.data.wireless_connection[i].sigmal_strength * 20);
-          const wirelessConnectionSignalStrengthMax = (homeData.data.wireless_connection[i].sigmal_strength_max * 20);
-          const wirelessConnectionType = ENVOY_API_CODE[homeData.data.wireless_connection[i].type] || 'undefined';
-          const wirelessConnectionConnected = homeData.data.wireless_connection[i].connected;
+             const wirelessConnectionSignalStrength = (homeData.data.wireless_connection[i].sigmal_strength * 20);
+             const wirelessConnectionSignalStrengthMax = (homeData.data.wireless_connection[i].sigmal_strength_max * 20);
+             const wirelessConnectionType = ENVOY_API_CODE[homeData.data.wireless_connection[i].type] || 'undefined';
+             const wirelessConnectionConnected = homeData.data.wireless_connection[i].connected;
+          
+             this.wirelessConnectionSignalStrength.push(wirelessConnectionSignalStrength);
+             this.wirelessConnectionSignalStrengthMax.push(wirelessConnectionSignalStrengthMax);
+             this.wirelessConnectionType.push(wirelessConnectionType);
+             this.wirelessConnectionConnected.push(wirelessConnectionConnected);
+          }
         }
 
         //enpower
