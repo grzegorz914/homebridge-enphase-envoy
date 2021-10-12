d502fbe75084e75c6a8feafa1f2495a8335c47e4
Grzegorz <grzegorz914@icloud.com>
2021-10-12 14:37:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2386,12 +2386,12 @@ class envoyPlatform {
   }
 
   configureAccessory(accessory) {
-    this.log.debug('configurePlatformAccessory');
+    this.log.debug('configureAccessory');
     this.accessories.push(accessory);
   }
 
   removeAccessory(accessory) {
-    this.log.debug('removePlatformAccessory');
+    this.log.debug('removeAccessory');
     this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
   }
 }
