7bc5363c70dd2cd4ce7e7502c3a3bae4eddb5e82
Grzegorz <grzegorz914@icloud.com>
2022-12-14 10:19:20 +0100

    code refactor

--- src/digestAuth.js
+++ src/digestAuth.js
@@ -8,39 +8,43 @@ class axiosDigestAuth {
         this.passwd = config.passwd;
     };
 
-    async request(options) {
-        let resHeaders;
-        let resMethod;
-        let count = 0;
+    request(options) {
+        return new Promise(async (resolve, reject) => {
+            let resHeaders;
+            let resMethod;
+            let count = 0;
 
-        try {
-            return await axios.request(options);
-        } catch (error) {
-            if (error.response === undefined || error.response.status !== 401 || !((resHeaders = error.response.headers["www-authenticate"]) === null || resHeaders === void 0 ? void 0 : resHeaders.includes('nonce'))) {
-                throw error;
-            };
-
-            const authDetails = error.response.headers['www-authenticate'].split(', ').map((v) => v.split('='));
-            const nonceCount = (`00000000${count++}`).slice(-8);
-            const cnonce = crypto.randomBytes(24).toString('hex')
-            const realm = authDetails.find((el) => el[0].toLowerCase().indexOf("realm") > -1)[1].replace(/"/g, '');
-            const nonce = authDetails.find((el) => el[0].toLowerCase().indexOf("nonce") > -1)[1].replace(/"/g, '');
-
-            const md5 = str => crypto.createHash('md5').update(str).digest('hex');
-            const HA1 = md5(`${this.user}:${realm}:${this.passwd}`);
-            const HA2 = md5(`${(resMethod = options.method) !== null && resMethod !== void 0 ? resMethod : "GET"}:${options.url}`)
-            const response = md5(`${HA1}:${nonce}:${nonceCount}:${cnonce}:auth:${HA2}`);
-            const authorization = `Digest username="${this.user}",realm="${realm}",nonce="${nonce}",uri="${options.url}",qop="auth",algorithm="MD5",response="${response}",nc="${nonceCount}",cnonce="${cnonce}"`;
-
-            if (options.headers) {
-                options.headers["authorization"] = authorization;
-            } else {
-                options.headers = {
-                    authorization
+            try {
+                const data = await axios.request(options);
+                resolve(data);
+            } catch (error) {
+                if (error.response === undefined || error.response.status !== 401 || !((resHeaders = error.response.headers["www-authenticate"]) === null || resHeaders === void 0 ? void 0 : resHeaders.includes('nonce'))) {
+                    reject(error);
                 };
+
+                const authDetails = error.response.headers['www-authenticate'].split(', ').map((v) => v.split('='));
+                const nonceCount = (`00000000${count++}`).slice(-8);
+                const cnonce = crypto.randomBytes(24).toString('hex')
+                const realm = authDetails.find((el) => el[0].toLowerCase().indexOf("realm") > -1)[1].replace(/"/g, '');
+                const nonce = authDetails.find((el) => el[0].toLowerCase().indexOf("nonce") > -1)[1].replace(/"/g, '');
+
+                const md5 = str => crypto.createHash('md5').update(str).digest('hex');
+                const HA1 = md5(`${this.user}:${realm}:${this.passwd}`);
+                const HA2 = md5(`${(resMethod = options.method) !== null && resMethod !== void 0 ? resMethod : "GET"}:${options.url}`)
+                const response = md5(`${HA1}:${nonce}:${nonceCount}:${cnonce}:auth:${HA2}`);
+                const authorization = `Digest username="${this.user}",realm="${realm}",nonce="${nonce}",uri="${options.url}",qop="auth",algorithm="MD5",response="${response}",nc="${nonceCount}",cnonce="${cnonce}"`;
+
+                if (options.headers) {
+                    options.headers["authorization"] = authorization;
+                } else {
+                    options.headers = {
+                        authorization
+                    };
+                };
+                const data = await axios.request(options);
+                resolve(data);
             };
-            return axios.request(options);
-        };
+        });
     };
 };
 
