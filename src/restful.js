d918652e7fdf16de75c24d7c4aa00af14ddcba58
Grzegorz <grzegorz914@icloud.com>
2023-04-01 14:14:32 +0200

    fix grid profile request in rest ful

--- src/restful.js
+++ src/restful.js
@@ -1,13 +1,12 @@
 "use strict";
 const express = require('express');
 const EventEmitter = require('events');
-const server = express();
 
 class RestFul extends EventEmitter {
     constructor(config) {
         super();
         this.restFulPort = config.port;
-        
+
         this.restFulData = {
             info: 'This data is not available in your system.',
             home: 'This data is not available in your system.',
@@ -16,13 +15,13 @@ class RestFul extends EventEmitter {
             metersReading: 'This data is not available in your system.',
             ensembleInventory: 'This data is not available in your system.',
             ensembleStatus: 'This data is not available in your system.',
-            profile: 'This data is not available in your system.',
+            gridProfile: 'This data is not available in your system.',
             liveData: 'This data is not available in your system.',
             production: 'This data is not available in your system.',
             productionCt: 'This data is not available in your system.',
             microinverters: 'This data is not available in your system.',
             powerMode: 'This data is not available in your system.',
-            plcLevel: 'This data is not available in your system.',
+            plcLevel: 'This data is not available in your system.'
         };
 
         this.connect();
@@ -30,22 +29,23 @@ class RestFul extends EventEmitter {
 
     connect() {
         try {
-            server.get('/info', (req, res) => { res.json(this.restFulData.info) });
-            server.get('/home', (req, res) => { res.json(this.restFulData.home) });
-            server.get('/inventory', (req, res) => { res.json(this.restFulData.inventory) });
-            server.get('/meters', (req, res) => { res.json(this.restFulData.meters) });
-            server.get('/metersreading', (req, res) => { res.json(this.restFulData.metersReading) });
-            server.get('/ensembleinventory', (req, res) => { res.json(this.restFulData.ensembleInventory) });
-            server.get('/ensemblestatus', (req, res) => { res.json(this.restFulData.ensembleStatus) });
-            server.get('/profile', (req, res) => { res.json(this.restFulData.profile) });
-            server.get('/livedata', (req, res) => { res.json(this.restFulData.liveData) });
-            server.get('/production', (req, res) => { res.json(this.restFulData.production) });
-            server.get('/productionct', (req, res) => { res.json(this.restFulData.productionCt) });
-            server.get('/microinverters', (req, res) => { res.json(this.restFulData.microinverters) });
-            server.get('/powermode', (req, res) => { res.json(this.restFulData.powerMode) });
-            server.get('/plclevel', (req, res) => { res.json(this.restFulData.plcLevel) });
+            const restFul = express();
+            restFul.get('/info', (req, res) => { res.json(this.restFulData.info) });
+            restFul.get('/home', (req, res) => { res.json(this.restFulData.home) });
+            restFul.get('/inventory', (req, res) => { res.json(this.restFulData.inventory) });
+            restFul.get('/meters', (req, res) => { res.json(this.restFulData.meters) });
+            restFul.get('/metersreading', (req, res) => { res.json(this.restFulData.metersReading) });
+            restFul.get('/ensembleinventory', (req, res) => { res.json(this.restFulData.ensembleInventory) });
+            restFul.get('/ensemblestatus', (req, res) => { res.json(this.restFulData.ensembleStatus) });
+            restFul.get('/gridprofile', (req, res) => { res.json(this.restFulData.gridProfile) });
+            restFul.get('/livedata', (req, res) => { res.json(this.restFulData.liveData) });
+            restFul.get('/production', (req, res) => { res.json(this.restFulData.production) });
+            restFul.get('/productionct', (req, res) => { res.json(this.restFulData.productionCt) });
+            restFul.get('/microinverters', (req, res) => { res.json(this.restFulData.microinverters) });
+            restFul.get('/powermode', (req, res) => { res.json(this.restFulData.powerMode) });
+            restFul.get('/plclevel', (req, res) => { res.json(this.restFulData.plcLevel) });
 
-            server.listen(this.restFulPort, () => {
+            restFul.listen(this.restFulPort, () => {
                 this.emit('connected', `RESTful started on port: ${this.restFulPort}`)
             });
 
@@ -77,8 +77,8 @@ class RestFul extends EventEmitter {
             case 'ensembleStatus':
                 this.restFulData.ensembleStatus = data;
                 break;
-            case 'profile':
-                this.restFulData.profile = data;
+            case 'gridProfile':
+                this.restFulData.gridProfile = data;
                 break;
             case 'liveData':
                 this.restFulData.liveData = data;
