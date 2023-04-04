53381482911a66b6e2c8896d5e6832fa1244eb97
Grzegorz <grzegorz914@icloud.com>
2023-04-04 08:56:21 +0200

    digest auth cleanup and better handle error

--- src/digestAuth.js
+++ src/digestAuth.js
@@ -10,40 +10,46 @@ class DigestAuth {
 
     request(options) {
         return new Promise(async (resolve, reject) => {
-            let resHeaders;
-            let resMethod;
             let count = 0;
 
             try {
                 const data = await axios.request(options);
                 resolve(data);
             } catch (error) {
-                if (error.response === undefined || error.response.status !== 401 || !((resHeaders = error.response.headers["www-authenticate"]) === null || resHeaders === void 0 ? void 0 : resHeaders.includes('nonce'))) {
-                    reject(`Digest authentication error: ${error}`);
+                const resHeaders = error.response.headers["www-authenticate"];
+                const resMethod = options.method;
+
+                if (error.response === undefined || error.response.status !== 401 || !(resHeaders === null || resHeaders === void 0 ? void 0 : resHeaders.includes('nonce'))) {
+                    reject(`Digest authentication response error: ${error}`);
                     return;
                 };
 
-                const authDetails = error.response.headers['www-authenticate'].split(', ').map((v) => v.split('='));
-                const nonceCount = (`00000000${count++}`).slice(-8);
-                const cnonce = crypto.randomBytes(24).toString('hex')
-                const realm = authDetails.find((el) => el[0].toLowerCase().indexOf("realm") > -1)[1].replace(/"/g, '');
-                const nonce = authDetails.find((el) => el[0].toLowerCase().indexOf("nonce") > -1)[1].replace(/"/g, '');
+                try {
+                    const authDetails = resHeaders.split(', ').map((v) => v.split('='));
+                    const nonceCount = (`00000000${count++}`).slice(-8);
+                    const cnonce = crypto.randomBytes(24).toString('hex')
+                    const realm = authDetails.find((el) => el[0].toLowerCase().indexOf("realm") > -1)[1].replace(/"/g, '');
+                    const nonce = authDetails.find((el) => el[0].toLowerCase().indexOf("nonce") > -1)[1].replace(/"/g, '');
 
-                const md5 = str => crypto.createHash('md5').update(str).digest('hex');
-                const HA1 = md5(`${this.user}:${realm}:${this.passwd}`);
-                const HA2 = md5(`${(resMethod = options.method) !== null && resMethod !== void 0 ? resMethod : "GET"}:${options.url}`)
-                const response = md5(`${HA1}:${nonce}:${nonceCount}:${cnonce}:auth:${HA2}`);
-                const authorization = `Digest username="${this.user}",realm="${realm}",nonce="${nonce}",uri="${options.url}",qop="auth",algorithm="MD5",response="${response}",nc="${nonceCount}",cnonce="${cnonce}"`;
+                    const md5 = str => crypto.createHash('md5').update(str).digest('hex');
+                    const HA1 = md5(`${this.user}:${realm}:${this.passwd}`);
+                    const HA2 = md5(`${resMethod !== null && resMethod !== void 0 ? resMethod : 'GET'}:${options.url}`)
+                    const response = md5(`${HA1}:${nonce}:${nonceCount}:${cnonce}:auth:${HA2}`);
+                    const authorization = `Digest username=${this.user},realm=${realm},nonce=${nonce},uri=${options.url},qop=auth,algorithm=MD5,response=${response},nc=${nonceCount},cnonce=${cnonce}`;
 
-                if (options.headers) {
-                    options.headers["authorization"] = authorization;
-                } else {
-                    options.headers = {
-                        authorization
+                    if (options.headers) {
+                        options.headers["authorization"] = authorization;
+                    } else {
+                        options.headers = {
+                            authorization
+                        };
                     };
+
+                    const data = await axios.request(options);
+                    resolve(data);
+                } catch (error) {
+                    reject(`Digest authentication data error: ${error}`);
                 };
-                const data = await axios.request(options);
-                resolve(data);
             };
         });
     };
