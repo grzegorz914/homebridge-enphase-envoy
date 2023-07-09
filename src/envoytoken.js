49a7b941310670681d7236c0c99ad47029d8aa96
Grzegorz <grzegorz914@icloud.com>
2023-07-09 11:02:30 +0200

    read file async/await

--- src/envoytoken.js
+++ src/envoytoken.js
@@ -24,79 +24,82 @@ class EnvoyToken {
 
     getToken() {
         return new Promise(async (resolve, reject) => {
-            const tokenFileExist = fs.readFileSync(this.tokenFile).length > 30;
+            try {
+                const tokenFileExist = await fsPromises.readFile(this.tokenFile).length > 30;
+                switch (tokenFileExist) {
+                    case true:
+                        const token = JSON.parse(fs.readFileSync(this.tokenFile));
+                        const tokenExpired = Math.floor(new Date().getTime() / 1000) > token.expires_at;
 
-            switch (tokenFileExist) {
-                case true:
-                    const token = JSON.parse(fs.readFileSync(this.tokenFile));
-                    const tokenExpired = Math.floor(new Date().getTime() / 1000) > token.expires_at;
+                        switch (tokenExpired) {
+                            case true:
+                                try {
+                                    //login to enlighten
+                                    const loginUrl = `${CONSTANS.EnphaseUrls.Login}?user[email]=${this.user}&user[password]=${this.password}`;
+                                    const loginData = await this.axiosInstanceLogin(loginUrl);
+                                    const cookie = loginData.headers['set-cookie'];
 
-                    switch (tokenExpired) {
-                        case true:
-                            try {
-                                //login to enlighten
-                                const loginUrl = `${CONSTANS.EnphaseUrls.Login}?user[email]=${this.user}&user[password]=${this.password}`;
-                                const loginData = await this.axiosInstanceLogin(loginUrl);
-                                const cookie = loginData.headers['set-cookie'];
+                                    //create axios instance token
+                                    const axiosInstanceToken = axios.create({
+                                        method: 'GET',
+                                        baseURL: CONSTANS.EnphaseUrls.BaseUrl,
+                                        headers: {
+                                            Accept: 'application/json',
+                                            Cookie: cookie
+                                        }
+                                    });
 
-                                //create axios instance token
-                                const axiosInstanceToken = axios.create({
-                                    method: 'GET',
-                                    baseURL: CONSTANS.EnphaseUrls.BaseUrl,
-                                    headers: {
-                                        Accept: 'application/json',
-                                        Cookie: cookie
-                                    }
-                                });
+                                    //get entrez token
+                                    const tokenUrl = `${CONSTANS.EnphaseUrls.EntrezAuthToken}?serial_num=${this.serialNumber}`;
+                                    const tokenData = await axiosInstanceToken(tokenUrl);
 
-                                //get entrez token
-                                const tokenUrl = `${CONSTANS.EnphaseUrls.EntrezAuthToken}?serial_num=${this.serialNumber}`;
-                                const tokenData = await axiosInstanceToken(tokenUrl);
+                                    //save token to the file
+                                    const token = tokenData.data;
+                                    await fsPromises.writeFile(this.tokenFile, JSON.stringify(token, null, 2));
 
-                                //save token to the file
-                                const token = tokenData.data;
-                                await fsPromises.writeFile(this.tokenFile, JSON.stringify(token, null, 2));
+                                    resolve(token);
+                                } catch (error) {
+                                    reject(error);
+                                }
+                                break;
+                            case false:
+                                resolve(token)
+                                break;
+                        }
+                        break;
+                    case false:
+                        try {
+                            //login to enlighten
+                            const loginUrl = `${CONSTANS.EnphaseUrls.Login}?user[email]=${this.user}&user[password]=${this.password}`;
+                            const loginData = await this.axiosInstanceLogin(loginUrl);
+                            const cookie = loginData.headers['set-cookie'];
 
-                                resolve(token);
-                            } catch (error) {
-                                reject(error);
-                            }
-                            break;
-                        case false:
-                            resolve(token)
-                            break;
-                    }
-                    break;
-                case false:
-                    try {
-                        //login to enlighten
-                        const loginUrl = `${CONSTANS.EnphaseUrls.Login}?user[email]=${this.user}&user[password]=${this.password}`;
-                        const loginData = await this.axiosInstanceLogin(loginUrl);
-                        const cookie = loginData.headers['set-cookie'];
+                            //create axios instance token
+                            const axiosInstanceToken = axios.create({
+                                method: 'GET',
+                                baseURL: CONSTANS.EnphaseUrls.BaseUrl,
+                                headers: {
+                                    Accept: 'application/json',
+                                    Cookie: cookie
+                                }
+                            });
 
-                        //create axios instance token
-                        const axiosInstanceToken = axios.create({
-                            method: 'GET',
-                            baseURL: CONSTANS.EnphaseUrls.BaseUrl,
-                            headers: {
-                                Accept: 'application/json',
-                                Cookie: cookie
-                            }
-                        });
+                            //get entrez token
+                            const tokenUrl = `${CONSTANS.EnphaseUrls.EntrezAuthToken}?serial_num=${this.serialNumber}`;
+                            const tokenData = await axiosInstanceToken(tokenUrl);
 
-                        //get entrez token
-                        const tokenUrl = `${CONSTANS.EnphaseUrls.EntrezAuthToken}?serial_num=${this.serialNumber}`;
-                        const tokenData = await axiosInstanceToken(tokenUrl);
+                            //save token to the file
+                            const token = tokenData.data;
+                            await fsPromises.writeFile(this.tokenFile, JSON.stringify(token, null, 2));
 
-                        //save token to the file
-                        const token = tokenData.data;
-                        await fsPromises.writeFile(this.tokenFile, JSON.stringify(token, null, 2));
-
-                        resolve(token);
-                    } catch (error) {
-                        reject(error);
-                    }
-                    break;
+                            resolve(token);
+                        } catch (error) {
+                            reject(error);
+                        }
+                        break;
+                }
+            } catch (error) {
+                reject(error);
             }
         });
     }
