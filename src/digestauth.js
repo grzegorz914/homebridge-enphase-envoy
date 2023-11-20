ab30a45db5c6ec5f8f34dae7c02af2c52448ac00
Grzegorz <grzegorz914@icloud.com>
2023-11-20 10:32:51 +0100

    cleanup

--- src/digestauth.js
+++ src/digestauth.js
@@ -6,6 +6,7 @@ class DigestAuth {
     constructor(config) {
         this.user = config.user;
         this.passwd = config.passwd;
+        this.count = 0;
     };
 
     request(path, options) {
@@ -17,23 +18,22 @@ class DigestAuth {
                 resolve(data);
             } catch (error) {
                 const resError = error.response;
-                if (resError === undefined || resError.status !== 401) {
-                    reject(`Digest authentication response error: ${resError}`);
+                if (!resError || resError.status !== 401) {
+                    reject(`Digest authentication response error: ${resError ? resError.status : 'Unknown error'}`);
                     return;
                 };
 
                 const resHeaders = resError.headers["www-authenticate"];
-                if (!(resHeaders === null || resHeaders === void 0 ? void 0 : resHeaders.includes('nonce'))) {
-                    reject(`Digest authentication headers error: ${resError.headers}`);
+                if (!(resHeaders || !resHeaders.includes('nonce'))) {
+                    reject(`Digest authentication headers error: ${resHeaders ? resHeaders : 'Header not found'}`);
                     return;
                 };
 
                 try {
-                    let count = 0;
                     const authDetails = resHeaders.split(', ').map((v) => v.split('='));
                     const realm = authDetails.find((el) => el[0].toLowerCase().includes("realm"))[1].replace(/"/g, '');
                     const nonce = authDetails.find((el) => el[0].toLowerCase().includes("nonce"))[1].replace(/"/g, '');
-                    const nonceCount = (`00000000${count++}`).slice(-8);
+                    const nonceCount = (`00000000${this.count++}`).slice(-8);
                     const cnonce = crypto.randomBytes(24).toString('hex');
                     const md5 = str => crypto.createHash('md5').update(str).digest('hex');
                     const HA1 = md5(`${this.user}:${realm}:${this.passwd}`);
@@ -41,12 +41,13 @@ class DigestAuth {
                     const response = md5(`${HA1}:${nonce}:${nonceCount}:${cnonce}:auth:${HA2}`);
 
                     //get data with digest authorization
-                    options.headers["authorization"] = `Digest username=${this.user},realm=${realm},nonce=${nonce},uri=${url},qop=auth,algorithm=MD5,response=${response},nc=${nonceCount},cnonce=${cnonce}`;
+                    const authHeaders = `Digest username=${this.user},realm=${realm},nonce=${nonce},uri=${url},qop=auth,algorithm=MD5,response=${response},nc=${nonceCount},cnonce=${cnonce}`;
+                    options.headers["authorization"] = authHeaders;
                     const data = await axios.request(url, options);
 
                     resolve(data);
                 } catch (error) {
-                    reject(`Digest authentication data error: ${error}`);
+                    reject(`Digest authentication error: ${error}`);
                 };
             };
         });
