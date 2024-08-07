d710e87f453489580d5f9b7eee09f25f6df6e589
Grzegorz <grzegorz914@icloud.com>
2024-08-07 14:40:19 +0200

    refresh timer state by state request

--- src/impulsegenerator.js
+++ src/impulsegenerator.js
@@ -13,10 +13,10 @@ class ImpulseGenerator extends EventEmitter {
             const timer = setInterval(() => {
                 this.emit(timerName);
             }, sampling);
-
             this.timers.push(timer);
         });
 
+        //update state
         this.timerState = true;
         this.emit('state', true);
     }
@@ -24,11 +24,14 @@ class ImpulseGenerator extends EventEmitter {
     stop() {
         this.timers.forEach(timer => clearInterval(timer));
         this.timers = [];
+
+        //update state
         this.timerState = false;
         this.emit('state', false);
     }
 
     state() {
+        this.emit('state', this.timerState);
         return this.timerState;
     }
 }
